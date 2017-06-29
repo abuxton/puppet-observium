083467b3b05d3fbe3d203d78e9e59887e4948299
Adam Buxton <buxton.adam@gmail.com>
2017-06-29 12:21:11 +0100

    encapsulated staging

--- manifests/install/community.pp
+++ manifests/install/community.pp
@@ -7,13 +7,13 @@ class observium::install::community (
   ) inherits observium::params {
 
   include ::staging
-
+if !defined(Class['staging']){
   class { 'staging' :
     path  => '/tmp/staging',
     owner => 'puppet',
     group => 'puppet',
   }
-
+}
   staging::file { 'observium-community-latest.tar.gz' :
     source => 'http://www.observium.org/observium-community-latest.tar.gz',
   }
