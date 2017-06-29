35ceda512f7d58448ec8e0c47b88989106d8efe7
Adam Buxton <buxton.adam@gmail.com>
2017-06-29 12:25:31 +0100

    refactor requires

--- manifests/install/db.pp
+++ manifests/install/db.pp
@@ -1,15 +1,15 @@
 class observium::install::db(
 
   ) inherits observium::params{
-    include ::mysql
+
     mysql_grant{'observium@localhost':
       user       => 'observium@localhost',
       table      => 'observium.*',
       privileges => ['ALL'],
-      require => Class['::mysql'],
     }
     exec{ '/opt/observium/discovery.php -u >> /opt/observium/logs/db-install.log 2 > &1'  :
       path    => '/usr/bin:/bin',
       creates => '/opt/observium/logs/db-install.log',
+      require => Mysql_grant['observium@localhost'],
     }
 }
