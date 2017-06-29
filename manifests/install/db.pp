612b6167841028a8121b084852b244bb2c668814
Adam Buxton <buxton.adam@gmail.com>
2017-06-29 15:36:06 +0100

    refacter params

--- manifests/install/db.pp
+++ manifests/install/db.pp
@@ -1,14 +1,15 @@
 class observium::install::db(
-
+  $dbuser       = $::observium::params::dbname,
+  $dbpassword   = $::observium::params::dbpassword,
   ) inherits observium::params{
     mysql::db { 'observium':
-    user     => 'observium',
-    password => 'observium',
+    user     => $dbuser,
+    password => $dbpassword',
     host     => 'localhost',
     grant    => ['ALL'],
 }
 
-    exec{ '/opt/observium/discovery.php -u >> /opt/observium/logs/db-install.log 2 > &1'  :
+    exec{ '/opt/observium/discovery.php -u >> /opt/observium/logs/db-install.log 2>&1'  :
       path    => '/usr/bin:/bin',
       creates => '/opt/observium/logs/db-install.log',
       require => Mysql::Db['observium'],
