54d793d0c5ad4385c7250bbd899a5950365f5096
Adam Buxton <buxton.adam@gmail.com>
2017-06-29 09:00:20 +0100

    simplifying deploy

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,15 +11,12 @@
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
 class observium (
-  Boolean $install_repos  = $::observium::params::install_repos,
-  Boolean $install_epel   = $::observium::params::install_epel,
+
 ) inherits ::observium::params {
 
   # validate parameters here
 
   class { '::observium::install':
-        install_repos => $install_repos,
-        install_epel  => $install_epel,
     } ->
   class { '::observium::config': } ~>
   class { '::observium::service': } ->
