09bea5d1f29f9dda89822145f818d1a4ae55ed5a
Adam Buxton <buxton.adam@gmail.com>
2017-06-29 12:39:32 +0100

    update mysql::db

--- manifests/install/db.pp
+++ manifests/install/db.pp
@@ -1,15 +1,16 @@
 class observium::install::db(
 
   ) inherits observium::params{
+    mysql::db { 'observium':
+    user     => 'observium',
+    password => 'observium',
+    host     => 'localhost',
+    grant    => ['ALL'],
+}
 
-    mysql_grant{'observium@localhost':
-      user       => 'observium@localhost',
-      table      => 'observium.*',
-      privileges => ['ALL'],
-    }
     exec{ '/opt/observium/discovery.php -u >> /opt/observium/logs/db-install.log 2 > &1'  :
       path    => '/usr/bin:/bin',
       creates => '/opt/observium/logs/db-install.log',
-      require => Mysql_grant['observium@localhost'],
+      require => Mysql::Db'observium'],
     }
 }
