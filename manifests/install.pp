21f361ceca494e3933b9b78f4ed0f645fc08660b
Adam Buxton <buxton.adam@gmail.com>
2017-06-29 15:54:24 +0100

    refactor to dynamically install location

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,17 +2,22 @@
 #
 class observium::install (
   $install_version = $observium::params::install_version
+  $install_location = $install_location,
   )inherits observium::params {
   # resources
 
   class { '::observium::install::packages' :
     } ->
   class { "::observium::install::${install_version}"  :
+    install_location => $install_location,
     } ->
   class { '::observium::install::logs'  :
+    install_location => $install_location,
     } ->
   class { '::observium::install::rrdtool' :
+    install_location => $install_location,
     } ->
   class { '::observium::install::db'  :
+    install_location => $install_location,
       }
 }
