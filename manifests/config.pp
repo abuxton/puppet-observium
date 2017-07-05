528d197c1668932e905ec28c0db77b236e622ad2
Adam Buxton <buxton.adam@gmail.com>
2017-07-05 14:47:46 +0100

    update push

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,10 +3,10 @@
 # This class is called from observium for service config.
 #
 class observium::config (
-    $::observium::config::install_location = $::observium::params::install_location,
+    $observium::config::install_location = $::observium::params::install_location,
   ) inherits observium::params {
   include ::observium::install
-  file {  "${::observium::config::install_location}/config.php" :
+  file {  "${observium::config::install_location}/config.php" :
     ensure  => file,
     source  => 'puppet:///modules/observium/config.php.default',
     require => Class['observium::install'],
